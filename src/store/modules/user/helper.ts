410ab0907b1099f0233146012360fb38aa448a03
miwu <1462063314@qq.com>
2023-04-20 15:24:47 +0800

    修改文字

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
       name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      description: '开启你的论文之旅',
     },
   }
 }
